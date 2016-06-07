5052222cd7847648a73b2bfa7cb01d05c74c4343
Ryan Wilkins <wilkinsrs@outlook.com>
2016-06-07 14:13:43 -0400

    add more detailed description

--- lists-and-strings/odd-positions.rb
+++ lists-and-strings/odd-positions.rb
@@ -1,4 +1,5 @@
 # Write a function that returns the elements in the odd positions of a list
+# Position in this case assumes the first element in the array has a position of 1 (from the perspective of someone viewing the list)
 
 def odd_positions(list)
     position=0
